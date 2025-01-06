2e82be43b18e476d53a74269549e2e807d5420d3
Gabi-Amaro <gads12@aluno.ifnmg.edu.br>
2025-01-05 21:38:06 -0300

    making post and get endpoints

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,18 +2,46 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use DateTimeZone;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
-    public function index(): JsonResponse
+    #[Route('/user', name: 'app_user', methods: ['GET'])]
+    public function index(EntityManagerInterface $entityManager): JsonResponse
     {
+        $repository = $entityManager->getRepository(User::class);
+
+        $users = $repository->findAll();
+
         return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/UserController.php',
+            'data' => $users,
         ]);
+    }
+
+    #[Route('/user', name: 'create_user', methods: ['POST'])]
+    public function create(Request $request, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $data = $request->request->all();
+
+        $user = new User();
+        $user->setName($data['name']);
+        $user->setEmail($data['email']);
+        $user->setPassword($data['password']);
+        $user->setCreatedAt(new \DateTimeImmutable('now', new DateTimeZone('America/Sao_Paulo')));
+        $user->setUpdatedAt(new \DateTimeImmutable('now', new DateTimeZone('America/Sao_Paulo')));
+
+        $entityManager->persist($user);
+
+        $entityManager->flush();
+
+        return $this->json([
+            'message' => 'usuario criado com o id '.$user->getId()
+        ], 201);
     }
 }
